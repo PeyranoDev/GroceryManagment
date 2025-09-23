76dbec58fa63efd77f9c5c6af03a68af1dd83e24
PeyranoDev <peyranoignacio@gmail.com>
2025-09-23 15:33:33 -0300

    feat: nueva migracion para only 1 grocery

--- backend/Infraestructure/Migrations/20250923183221_onlyonegrocery.Designer.cs
+++ backend/Infraestructure/Migrations/20250923183221_onlyonegrocery.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Infraestructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Infraestructure.Migrations
 {
     [DbContext(typeof(GroceryManagmentContext))]
-    partial class GroceryManagmentContextModelSnapshot : ModelSnapshot
+    [Migration("20250923183221_onlyonegrocery")]
+    partial class onlyonegrocery
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/Infraestructure/Migrations/20250923183221_onlyonegrocery.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class onlyonegrocery : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GroceryId",
+                table: "Users",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "Users",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_GroceryId",
+                table: "Users",
+                column: "GroceryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Groceries_GroceryId",
+                table: "Users",
+                column: "GroceryId",
+                principalTable: "Groceries",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Groceries_GroceryId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_GroceryId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "GroceryId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+        }
+    }
+}
